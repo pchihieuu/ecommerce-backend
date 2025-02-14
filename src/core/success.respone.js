dabecfeeacad81fc918fa0d6c494b48b2c3b4e52
pchieu <email-hieup3611@gmail.com>
2025-02-14 21:25:44 +0700

    chore: make API response use clas

---
+++ src/core/success.respone.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const StatusCode = {
+  OK: 200,
+  CREATED: 201,
+};
+
+const ReasonStatusCode = {
+  OK: "Success",
+  CREATED: "Created",
+};
+
+class SuccessRespone {
+  constructor({
+    message,
+    statusCode = StatusCode.OK,
+    reasonStatusCode = ReasonStatusCode.OK,
+    metadata = {},
+  }) {
+    this.message = !message ? reasonStatusCode : message;
+    this.status = statusCode;
+    this.metadata = metadata;
+  }
+
+  send(res, headers = {}) {
+    return res.status(this.status).json(this);
+  }
+}
+
+class OK extends SuccessRespone {
+  constructor({ message, metadata = {} }) {
+    super({ message, metadata });
+  }
+}
+
+class CREATED extends SuccessRespone {
+  constructor({ message, metadata = {}, options = {} }) {
+    super({
+      message,
+      statusCode: StatusCode.CREATED,
+      reasonStatusCode: ReasonStatusCode.CREATED,
+      metadata,
+    });
+    this.options = options;
+  }
+}
+
+module.exports = {
+  OK,
+  CREATED,
+};
