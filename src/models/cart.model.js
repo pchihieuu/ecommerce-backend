238c0b4f5f25f23b1d6e5da37da0aed7ae7debda
pchieu <email-hieup3611@gmail.com>
2025-02-24 20:12:31 +0700

    feat: implement cart service

---
+++ src/models/cart.model.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const { model, Schema, Types } = require("mongoose");
+
+const DOCUMENT_NAME = "Cart";
+const COLLECTION_NAME = "Carts";
+
+const cartSchema = new Schema(
+  {
+    cart_products: {
+      type: Array,
+      required: true,
+      default: [],
+      /*
+        [
+            {
+                product_id,
+                shop_id,
+                quantity,
+                price,
+                name
+            }
+        ]
+     */
+    },
+    cart_state: {
+      type: String,
+      required: true,
+      enum: ["ACTIVE", "COMPLETED", "FAILED", "PENDING"],
+      default: "ACTIVE",
+    },
+    cart_count_product: {
+      type: Number,
+      default: 0,
+    },
+    cart_user_id: {
+      type: Types.ObjectId,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    // timeseries: {
+    //   createdAt: "createdOn",
+    //   updatedAt: "modifiedOn",
+    // },
+    collection: COLLECTION_NAME,
+  }
+);
+
+module.exports = model(DOCUMENT_NAME, cartSchema);
