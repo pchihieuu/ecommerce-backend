13a75f33118b69b5aa82993b74328b4fa6b3d49a
pchieu <email-hieup3611@gmail.com>
2025-02-28 14:22:27 +0700

    feat: implement service push log router (#61)

--- src/loggers/discord.log.js
+++ src/loggers/discord.log.js
@@ -1,26 +1,54 @@
 "use strict";
 
-require("dotenv").config();
 const { Client, GatewayIntentBits } = require("discord.js");
-const client = new Client({
-  intents: [
-    GatewayIntentBits.DirectMessages,
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent,
-  ],
-});
+require("dotenv").config();
+const { CHANNEL_ID, TOKEN_DISCORD } = process.env;
 
-client.on("ready", () => {
-  console.log(`Logged is as  ${client.user.tag}`);
-});
-const token = process.env.TOKEN_DISCORD_BOT;
-
-client.login(token);
-
-client.on("messageCreate", (msg) => {
-  if (msg.author.bot) return;
-  if (msg.content.toLocaleLowerCase() === "hello") {
-    msg.reply(`Hello, can I help you!`);
+class LoggerService {
+  constructor() {
+    this.client = new Client({
+      intents: [
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+      ],
+    });
+    // add channelId
+    this.channelId = CHANNEL_ID;
+    this.client.on("ready", () => {
+      console.log(`Logged is as ${this.client.user.tag}`);
+    });
+    this.client.login(TOKEN_DISCORD);
   }
-});
+
+  sendToMessage(message = "message") {
+    const channel = this.client.channels.cache.get(this.channelId);
+    if (!channel) {
+      console.error(`Could not find the channel...`, this.channelId);
+      return;
+    }
+    channel.send(message).catch((e) => console.error(e));
+  }
+
+  sendToFormatCode(logData) {
+    const {
+      code,
+      message = "This is some additional information about the code",
+      title = "Code Example",
+    } = logData;
+    const codeMessage = {
+      content: message,
+      embeds: [
+        {
+          color: parseInt("00ff00", 16),
+          title,
+          description: "```json\n" + JSON.stringify(code, null, 2) + "\n ```",
+        },
+      ],
+    };
+    this.sendToMessage(codeMessage);
+  }
+}
+
+module.exports = new LoggerService();
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,9 @@
 
 const express = require("express");
 const { apiKey, permission } = require("../auth/checkAuth");
+const { pushToLogDiscord } = require("../middlewares/index.js");
 const router = express.Router();
-
+router.use(pushToLogDiscord);
 // check api key
 router.use(apiKey);
 
@@ -16,4 +17,5 @@ router.use("/v1/api/discount", require("./discount"));
 router.use("/v1/api/cart", require("./cart"));
 router.use("/v1/api/checkout", require("./checkout"));
 router.use("/v1/api/inventory", require("./inventory"));
+
 module.exports = router;
