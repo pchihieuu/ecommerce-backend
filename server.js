930ea025fa9ebff460e2a5e2481fdf6b41483de6
pchieu <email-hieup3611@gmail.com>
2025-03-15 20:39:43 +0700

    fix: missing env file

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const app = require("./src/app");
 
 const port = process.env.DEV_APP_PORT || 3056;
-
+const dotenv = require("dotenv");
+dotenv.config();
 const server = app.listen(port, () => {
   console.log(`Server start with ${port}`);
 });
