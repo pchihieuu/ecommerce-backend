cf4c52b51d0787c89d84de5838cdfe9ec28b6f19
pchieu <email-hieup3611@gmail.com>
2025-03-16 21:33:24 +0700

    fix: change code with redis version 4.x.x

--- src/services/redisPubSub.service.js
+++ src/services/redisPubSub.service.js
@@ -1,3 +1,65 @@
+// "use strict";
+// const redis = require("redis");
+
+// class RedisPubSubService {
+//   constructor() {
+//     this.subscriber = redis.createClient();
+//     this.publisher = redis.createClient();
+//   }
+
+//   async connectClients() {
+//     try {
+//       if (!this.publisher.isOpen) {
+//         console.log(">>> connected publisher");
+//         await this.publisher.connect();
+//       }
+//       if (!this.subscriber.isOpen) {
+//         console.log(">>> connected subscriber");
+//         await this.subscriber.connect();
+//       }
+//     } catch (error) {
+//       console.log("Error connecting to Redis:", error);
+//       throw error;
+//     }
+//   }
+
+//   async publish(channel, message) {
+//     await this.connectClients();
+//     return new Promise((resolve, reject) => {
+//       this.publisher.publish(channel, message, (err, reply) => {
+//         if (err) {
+//           console.log("Error in publish:", err);
+//           reject(err);
+//         } else {
+//           resolve(reply);
+//         }
+//       });
+//     });
+//   }
+//   async subscribe(channel, callback) {
+//     await this.connectClients();
+//     this.subscriber.subscribe(channel);
+//     this.subscriber.on("message", (subscriberChannel, message) => {
+//       if (channel === subscriberChannel) {
+//         callback(channel, message);
+//       }
+//     });
+//   }
+
+//   // Add a method for cleanup/disconnection if needed
+//   async disconnect() {
+//     try {
+//       await this.publisher.quit();
+//       await this.subscriber.quit();
+//       console.log("Redis clients disconnected.");
+//     } catch (error) {
+//       console.log("Error disconnecting Redis clients:", error);
+//     }
+//   }
+// }
+
+// module.exports = new RedisPubSubService();
+
 "use strict";
 const redis = require("redis");
 
@@ -5,6 +67,7 @@ class RedisPubSubService {
   constructor() {
     this.subscriber = redis.createClient();
     this.publisher = redis.createClient();
+    this.subscriptions = new Map();
   }
 
   async connectClients() {
@@ -25,28 +88,37 @@ class RedisPubSubService {
 
   async publish(channel, message) {
     await this.connectClients();
-    return new Promise((resolve, reject) => {
-      this.publisher.publish(channel, message, (err, reply) => {
-        if (err) {
-          console.log("Error in publish:", err);
-          reject(err);
-        } else {
-          resolve(reply);
-        }
-      });
-    });
-  }
-  async subscribe(channel, callback) {
-    await this.connectClients();
-    this.subscriber.subscribe(channel);
-    this.subscriber.on("message", (subscriberChannel, message) => {
-      if (channel === subscriberChannel) {
-        callback(channel, message);
-      }
-    });
+    try {
+      const reply = await this.publisher.publish(channel, message);
+      return reply;
+    } catch (err) {
+      console.log("Error in publish:", err);
+      throw err;
+    }
   }
 
-  // Add a method for cleanup/disconnection if needed
+  async subscribe(channel, callback) {
+    await this.connectClients();
+
+    // Lưu callback vào Map để có thể tham chiếu sau này
+    this.subscriptions.set(channel, callback);
+
+    try {
+      // Đăng ký subscriber với callback bọc
+      await this.subscriber.subscribe(channel, (message, channel) => {
+        const cb = this.subscriptions.get(channel);
+        if (cb) {
+          cb(channel, message);
+        }
+      });
+
+      console.log(`Subscribed to channel: ${channel}`);
+    } catch (error) {
+      console.log("Error in subscribe:", error);
+      throw error;
+    }
+  }
+
   async disconnect() {
     try {
       await this.publisher.quit();
